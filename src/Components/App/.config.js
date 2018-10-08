32094814432206bf546034f0cfa2e7aee9b89ecb
Nejc Vukovic <nejc.vukovic@nvwebd.com>
2018-10-09 01:19:27 +0200

    add initial components, styles and flickr key

---
+++ src/Components/App/.config.js
@@ -0,0 +1,2 @@
+const apiKey = "347cebc0e0adff1a2d3776fb1bd728fa";
+export default apiKey;
--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react';
-import logo from '../../logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import apiKey from "./.config";
+
+// import logo from "../../logo.svg";
+
+import "./App.css";
+import SearchBar from "../SearchBar/SearchBar";
+import Nav from "../Nav/Nav";
+import Gallery from "../Gallery/Gallery";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <SearchBar />
+        <Nav />
+        <Gallery />
+      </>
     );
   }
 }
